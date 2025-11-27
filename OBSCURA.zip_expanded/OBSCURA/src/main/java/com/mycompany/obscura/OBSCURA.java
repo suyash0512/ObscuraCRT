bececb45f141aa26bd4f08849b4c67ea6c19fb40
Suyash Gupta <75306618+suyash0512@users.noreply.github.com>
2025-11-27 11:52:16 +0530

    fixed padding

--- OBSCURA.zip_expanded/OBSCURA/src/main/java/com/mycompany/obscura/OBSCURA.java
+++ OBSCURA.zip_expanded/OBSCURA/src/main/java/com/mycompany/obscura/OBSCURA.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class OBSCURA extends JFrame {
 
     private final JLabel originalPreview = new JLabel("Original Image", SwingConstants.CENTER);
