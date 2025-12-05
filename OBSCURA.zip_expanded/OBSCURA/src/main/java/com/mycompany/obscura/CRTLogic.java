a6ca1aa5f012d3c8b6360e7d305fdd391340920d
Suyash Gupta <75306618+suyash0512@users.noreply.github.com>
2025-12-05 12:05:10 +0530

    updated logic with some advancements

--- OBSCURA.zip_expanded/OBSCURA/src/main/java/com/mycompany/obscura/CRTLogic.java
+++ OBSCURA.zip_expanded/OBSCURA/src/main/java/com/mycompany/obscura/CRTLogic.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 public class CRTLogic {
+
     
     private List<Integer> key = new ArrayList<>();
 
@@ -94,6 +95,7 @@ public class CRTLogic {
         return 10 * Math.log10((255 * 255) / mse);
     }
 }
+
 
 
 
